48de4f2018d814c909da5474f83b9adfbba0d67b
Nicolas Gabriel Bustamante Lopes <nicolasgabrielbustamantelopes@gmail.com>
2024-08-29 09:23:58 -0300

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,52 @@
-import React from "react";
 import { useState } from "react";
-import Modal from "react-modal";
 import Logo from "../assets/images/logo.svg";
-import "./../css/Navbar.css";
-import Login from "./Login";
-import { IoCloseSharp } from "react-icons/io5";
+import { links } from "../data";
+import Button from "./button";
 import { IoMdMenu } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Navbar = () => {
-  const [AbrirMenu, setAbrirMenu] = useState(false);
-  const [AbrirLogin, setAbrirLogin] = useState(false);
-
+  const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => {
-    setAbrirMenu(!AbrirMenu);
+    setMenuOpen(!menuOpen);
   };
-  
   return (
     <>
-      <nav className="navbar-container">
-        <img src={Logo} alt="Logo" className="logo" />
-        <div className="navbar-input-pesquisa">
-          <input type="text" placeholder="busque aqui" />
-        </div>
-        <div className="navbar-menu-container">
-          <a href="./../pages/Eventos.jsx">Eventos</a>
-          <a href="./../pages/Sobre.jsx">Sobre Nós</a>
-          <a href="./../pages/Organizacao.jsx">Sou uma organização</a>
-        </div>
-        <button type="button" onClick={() => setAbrirLogin(true)}>Entrar</button>
-        <button className="navbar-menu-button" onClick={toggleMenu}>
-          {AbrirMenu ? <IoCloseSharp /> : <IoMdMenu />}
+      <nav className="flex w-full items-center md:justify-around justify-between p-3 bg-blue-900 text-white">
+        <img src={Logo} className="" />
+        <ul className="md:flex hidden gap-10 md:flex-row">
+          {links.map((link) => (
+            <li
+              key={link.id}
+              className="cursor-pointer hover:text-orange-500 transition ease-in duration-300"
+            >
+              {link.title}
+            </li>
+          ))}
+        </ul>
+        <Button className="md:block hidden" />
+        <button className="block md:hidden text-[28px]" onClick={toggleMenu}>
+          {menuOpen ? <IoCloseSharp /> : <IoMdMenu />}
         </button>
       </nav>
-      {AbrirMenu && (
-        <div className="navbar-sidebar">
-          <ul>
-            <li><a href="./../pages/Eventos.jsx">Eventos</a></li>
-            <li><a href="./../pages/Sobre.jsx">Sobre Nós</a></li>
-            <li><a href="./../pages/Organizacao.jsx">Sou uma organização</a></li>
-          </ul>
+      {menuOpen && (
+        <div className="fixed inset-0 flex justify-center h-screen w-screen backdrop-blur-sm mt-[48px]">
+          <div className="bg-white h-[250px] w-[340px] max-w-md flex items-center justify-center rounded-r-lg shadow-md mt-[48px]">
+            <ul className="flex flex-col gap-6">
+              {links.map((link) => (
+                <li
+                  key={link.id}
+                  className="cursor-pointer hover:text-orange-500 transition ease-in duration-50"
+                >
+                  {link.title}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       )}
-      <Modal
-        isOpen = {AbrirLogin}
-        onRequestClose={() => setAbrirLogin(false)}
-        style={{
-          overlay: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-          content: {
-            inset: 'auto',
-            position: 'relative',
-            top: 50,
-            borderRadius: 32,
-            textAlign: 'center',
-            backgroundColor: '#FBFBFE', 
-            width: 500,
-          }
-        }}
-      >
-        <Login fecharLogin = {() => setAbrirLogin(false)}></Login>
-      </Modal>
     </>
   );
 };
 
 export default Navbar;
-
-
