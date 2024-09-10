a44a827ebc376fd355730e084f412db597be54d2
Anderson Brito Santos <141651296+andersonbs96@users.noreply.github.com>
2024-09-10 11:49:15 -0300

    Add files via upload

--- src/components/DadosPessoal.jsx
+++ src/components/DadosPessoal.jsx
@@ -1,89 +1,151 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const DadosPessoal = () => {
-    const [DadosPagina, setDadosPagina] = useState(1);
+    const [dadosNome, setDadosNome] = useState("");
+    const [dadosSobrenome, setDadosSobrenome] = useState("");
+    const [dadosCPF, setDadosCPF] = useState("");
+    const [dadosDataNasc, setDadosDataNasc] = useState("");
+    const [dadosDDD, setDadosDDD] = useState("");
+    const [dadosTelefone, setDadosTelefone] = useState("");
+    const [dadosCEP, setDadosCEP] = useState("");
+    const [dadosEndereco, setDadosEndereco] = useState("");
+    const [dadosNumero, setDadosNumero] = useState("");
+    const [dadosBairro, setDadosBairro] = useState("");
+    const [dadosCidade, setDadosCidade] = useState("");
+    const [dadosEstado, setDadosEstado] = useState("");
+    
+    const [sucesso, setSucesso] = useState(false);
+    const [erro, setErro] = useState(false);
 
-    const proxPasso = () => {
-        setDadosPagina(DadosPagina + 1)
+    const Estado = () => {
+        return [
+            "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", 
+            "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", 
+            "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+        ];
+    };
+
+    const buscarCEP = async (e) => {
+        e.preventDefault();
+        const cep = dadosBairro
+
+        if (cep.length === 8){
+            try {
+                const response = await fetch(`viacep.com.br/ws/${cep}/json/`)
+                const data = await response.json();
+
+                if (! data.erro) {
+                    setDadosEndereco(data.logradouro);
+                    setDadosBairro(data.bairro);
+                    setDadosCidade(data.localidade);
+                    setDadosEstado(data.uf);
+                }
+                else {
+                    alert("CEP não encontrado.");
+                }
+            } 
+            catch (error) {
+                alert("Erro ao buscar CEP. Tente novamente.");
+            }
+        } 
+        else {
+            alert("CEP inválido.");
+        }
     }
 
-    const antPasso = () => {
-        setDadosPagina(DadosPagina - 1)
-    }
-
-    const PassoPagina = () => {
-        switch (DadosPagina) {
-            case 1:
-                return (
-                    <div>
-                        <h3>Passo 1</h3>
-                        <div>
-                            <label htmlFor="dados-nome">Nome</label>
-                            <input id="dados-nome" type="text"/>
-                            <label htmlFor="dados-sobrenome">Sobrenome</label>
-                            <input id="dados-sobrenome" type="text"/>
-                        </div>
-                        <div>
-                            <label htmlFor="dados-cpf">CPF</label>
-                            <input id="dados-cpf" type="text" />
-                        </div>
-                        <div>
-                            <label htmlFor="dados-data-nasc">Data de Nascimento</label>
-                            <input id="dados-data-nasc" type="date"/>
-                        </div>
-                        <div>
-                            <label htmlFor="dados-ddd">DDD</label>
-                            <input id="dados-ddd" type="text"/>
-                            <label htmlFor="dados-telefone">Telefone</label>
-                            <input id="dados-telefone" type="text"/>
-                        </div>
-                        <div>
-                            <button onClick={proxPasso}>Próximo</button>
-                        </div>
-                    </div>
-                );
-            case 2:
-                return (
-                    <div>
-                        <h3>Passo 2</h3>
-                        <div>
-                            <label htmlFor="dados-cep">CEP</label>
-                            <input id="dados-cep" type="dados-cep"/>
-                            <button onClick={buscarCEP}></button>
-                        </div>
-                        <div>
-                            <label htmlFor="dados-endereco">Endereço</label>
-                            <input id="dados-endereco" type="text"/>
-                            <label htmlFor="dados-numero">Numero</label>
-                            <input id="dados-numero" type="text"/>
-                        </div>
-                        <div>
-                            <label htmlFor="dados-bairro">Bairro</label>
-                            <input id="dados-bairro" type="text"/>
-                        </div>
-                        <div>
-                            <label htmlFor="dados-cidade">Cidade</label>
-                            <input id="dados-cidade" type="text"/>
-                            <label htmlFor="dados-estado">UF</label>
-                            <select id="dados-estado">
-                                <option>-- Selecione o estado --</option>
-                            </select>
-                        </div>
-                        <div>
-                            <button onClick={antPasso}>Voltar</button>
-                            <button onClick={proxPasso}>Cadastrar</button>
-                        </div>
-                    </div>
-                );
-            default: 
-                return null;
+    const EnviarDados = async (e) => {
+        e.preventDefault();
+        if (!dadosNome || !dadosSobrenome || !dadosCPF || !dadosDataNasc || !dadosDDD || !dadosTelefone) {
+            try {
+                const response = await axios.post("https:\\volun-api-eight.vercel.app/usuario", {
+                    nome : dadosNome,
+                    sobrenome : dadosSobrenome,
+                    cpf : dadosCPF,
+                    dataNasc : dadosDataNasc,
+                    ddd : dadosDDD,
+                    telefone : dadosTelefone
+                });
+                
+                if (response.status === 201) {
+                    setSucesso(true);
+                    setErro(false);
+                    // Optionally, reset the form or close the modal
+                }
+            }
+            catch (error){
+                setSucesso(true);
+                setErro(false);
             }
         }
-    return (
+        else {
+            setErro(true);
+            setSucesso(false);
+        }
+    }
+
+    return(
         <>
-            <div>{PassoPagina()}</div>
+            <div className="dados-container">
+                <div className="dados-pessoal-container">
+                    <h4>Insira os dados pessoais</h4>
+                    <div>
+                        <label htmlFor="dadosNome">Nome: </label>
+                        <input type="text" name="dadosNome" value={dadosNome} onChange={(e) => setDadosNome(e.target.value)} />
+                        <label htmlFor="dadosSobrenome">Sobrenome: </label>
+                        <input type="text" name="dadosSobrenome" value={dadosSobrenome} onChange={(e) => setDadosSobrenome(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="dadosCPF">CPF: </label>
+                        <input type="text" name="dadosCPF" value={dadosCPF} onChange={(e) => setDadosCPF(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="dadosDataNasc">Data de Nascimento: </label>
+                        <input type="date" name="dadosDataNasc" value={dadosDataNasc} onChange={(e) => setDadosDataNasc(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="dadosDDD">DDD: </label>
+                        <input type="text" name="dadosDDD" value={dadosDDD} onChange={(e) => setDadosDDD(e.target.value)}/>
+                        <label htmlFor="dadosTelefone">Telefone: </label>
+                        <input type="text" name="dadosTelefone" value={dadosTelefone} onChange={(e) => setDadosTelefone(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="dados-endereco-container">
+                    <div>
+                        <label htmlFor="dadosCEP">CEP: </label>
+                        <input type="text" name="dadosCEP" value={dadosCEP} onChange={(e) => setDadosCEP(e.target.value)}/>
+                        <button type="submit" onClick={buscarCEP}>Buscar</button>
+                    </div>
+                    <div>
+                        <label htmlFor="dadosEndereco">Endereço: </label>
+                        <input type="text" name="dadosEndereco" value={dadosEndereco} onChange={(e) => setDadosEndereco(e.target.value)}/>
+                        <label htmlFor="dadosNumero">Numero: </label>
+                        <label type="text" name="dadosNumero" value={dadosNumero} onChange={(e) => setDadosNumero(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="dadosBairro">Bairro: </label>
+                        <input type="text" name="dadosBairro" value={dadosBairro} onChange={(e) => setDadosBairro(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="dadosCidade">Cidade: </label>
+                        <input type="text" name="dadosCidade" value={dadosCidade} onChange={(e) => setDadosCidade(e.target.value)}/>
+                        <label htmlFor="dadosEstado">Estado: </label>
+                        <select name="dadosEstado" value={dadosEstado} onChange={(e) => setDadosEstado}>
+                            <option>-- selecione o estado --</option>
+                            {Estado().map((estado, index) => (
+                                <option key={index} value={estado}>
+                                    {estado}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <button type="submit" onClick={EnviarDados}>Cadastrar</button>
+                    </div>
+                </div>
+            </div>
         </>
-    );
+    );    
 }
 
 export default DadosPessoal;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import Cadastrar from './components/Cadastrar.jsx'
+import DadosPessoal from './components/DadosPessoal.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render (
@@ -12,6 +13,7 @@ root.render (
       <Routes>
         <Route path='/' element={<App />}/>
         <Route path='/cadastrar' element={<Cadastrar />}/>
+        <Route path='/dados' element={<DadosPessoal/>}/>
       </Routes>
     </Router>
   </React.StrictMode>
