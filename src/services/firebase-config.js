59f488416b741006d01e9bb0903d1e1b632fc4f2
Nicolas Gabriel Bustamante Lopes <nicolasgabrielbustamantelopes@gmail.com>
2024-09-11 10:37:40 -0300

    Update firebase-config.js

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Suas credenciais do Firebase (agora protegidas via variáveis de ambiente)
+// Suas credenciais do Firebase (Firebase Config)
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDqzE1nUwoE7BTRcoLetqnr-1JhRXyDAPg",
+  authDomain: "volun-api.firebaseapp.com",
+  projectId: "volun-api",
+  storageBucket: "volun-api.appspot.com",
+  messagingSenderId: "276715044615",
+  appId: "1:276715044615:web:d78b93fecef21125124fa2",
+  measurementId: "G-61090CTK4V"
 };
 
 // Inicializando o Firebase
