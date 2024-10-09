d17cb265c62d233dac94415aacea23a9040fa31b
Lucas Santana <162702279+LucasSantta@users.noreply.github.com>
2024-10-09 09:21:23 -0300

    Add files via upload

--- src/components/modera/components/usuarios.jsx
+++ src/components/modera/components/usuarios.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../components/css/Usuario.css';
+import Modal from "react-modal";
 
 const Usuarios = () => {
   const [usuarios, setUsuarios] = useState([]);
@@ -33,6 +34,17 @@ const Usuarios = () => {
     return <div>Erro: {error}</div>;
   }
 
+  // Função para os botões
+  const handleDelete = (usuario) => {
+    console.log(`Excluir usuário: ${usuario.nome}`);
+    // Em breve implementar a função de usuarios da API
+  };
+
+  const handleAdvertir = (usuario) => {
+    console.log(`Advertir usuário: ${usuario.nome}`);
+    // Em breve implemetar a função de Adverter o usuario
+  };
+
   return (
     <div className='lista-usuario'>
       <h1>Página de Usuários</h1>
@@ -44,6 +56,7 @@ const Usuarios = () => {
             <th>DDD</th>
             <th>Telefone</th>
             <th>Data de Nascimento</th>
+            <th>Ações</th> 
           </tr>
         </thead>
         <tbody>
@@ -54,6 +67,29 @@ const Usuarios = () => {
               <td>{usuario.ddd}</td>
               <td>{usuario.telefone}</td>
               <td>{usuario.data_nascimento}</td>
+              <td>
+                <button 
+                  onClick={() => handleDelete(usuario)} 
+                  style={{ background: 'none', border: 'none', cursor: 'pointer', padding: 0 }}
+                >
+                  <img 
+                    src='src/components/modera/assets/img/image 34.png' 
+                    alt='Excluir' 
+                    className='icon' 
+                    style={{ marginRight: '10px' }} 
+                  />
+                </button>
+                <button 
+                  onClick={() => handleAdvertir(usuario)} 
+                  style={{ background: 'none', border: 'none', cursor: 'pointer', padding: 0 }}
+                >
+                  <img 
+                    src='src/components/modera/assets/img/image 33.png' 
+                    alt='Advertir' 
+                    className='icon' 
+                  />
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
@@ -63,4 +99,5 @@ const Usuarios = () => {
 };
 
 export default Usuarios;
+
 
