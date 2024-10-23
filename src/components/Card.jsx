6201cd90c64c03d03ce6736d6cf8aaaf327528d6
KevinJota <141651714+KevinJota@users.noreply.github.com>
2024-10-22 23:13:20 -0300

    fix Card/ update Eventos.jsx
    Arrumei o erro gerado pelo Card e atualizei a page Eventos para renderizar o Component Loader enquanto busca Dados de eventos na API

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -56,7 +56,7 @@ const Card = ({ titulo, descricao, ongNome, dataInicio, imgUrl, vagaLimite }) =>
                     </span>
                 </div>
             </div>
-            <button className="card-details" onClick={MudaCard}>
+            <button className="card-details-x" onClick={MudaCard}>
                 <p><Link to="/detalhes_eventos">Ver detalhes</Link></p>
                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
                     <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
