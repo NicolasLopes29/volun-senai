5a4c9be269da70269e2ae1fdcdd05c427dafe0fc
Heitor De Melo Muniz <141651619+HeitorMuniz16@users.noreply.github.com>
2024-11-19 11:50:26 -0300

    upando a criação eventos
    faltam apenas 2 coisas pra arrumar

--- src/components/CriacaoEvento.jsx
+++ src/components/CriacaoEvento.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./../css/CriacaoEventos.css";
 import calendario from "./../assets/images/calendario.svg";
 import pessoas from "./../assets/images/icon-pessoas.svg";
+import correto from "./../assets/images/icon-correto.svg";
+import cancela from "./../assets/images/icon-cancela.svg";
 
 
 const CriacaoEventos = () =>{
@@ -14,12 +16,12 @@ const CriacaoEventos = () =>{
             <h1>Novo Evento+</h1>
         </div> 
 
-        <div className="toda-pagina">
+        <div className="toda-pagina2">
             {/* Primeira parte vai aqui */}
             <div className="primera-parte">
-                <p>Digite o nome do evento</p>
+                <p className="nome-bonito">Digite o nome do evento</p>
                     <input placeholder="Nome do evento" className="nome-evento"></input>
-                    <p>selecione as tags do evento</p>
+                    <p className="titulo-tags">Selecione as tags do evento</p>
                     <select className="tags-evento">Classificação de eventos
                         <option>termas</option>
                         <option>do vale</option>
@@ -35,10 +37,12 @@ const CriacaoEventos = () =>{
                 <div className="background-colar-img">{}cole a imagem aqui +</div>
                 <div className="definir-vagas">
                     <p>Defina a quantidade de pessoas</p>
+                    <img src= {pessoas} alt="quantidade de pessoas" className="icon-pessoas2"></img>
                     <input className="numero-vagas"></input>
                 </div>
                 <div className="definir-dia">
                     <p>Defina os dias do evento</p>
+                    <img src={calendario} alt="dia do evento" className="icon-calendario2"></img>
                     <input className="data-dia-evento"></input>
                 </div>
                 <div>
@@ -46,11 +50,19 @@ const CriacaoEventos = () =>{
                 </div>
             </div>
         </div>
-        <div className="Endereço-mapa"></div>
+
+        <div className="endereco-mapa2">Mapa foda vem aqui</div>
 
         <div className="botões-evento">
-            <button className="cancelar-evento">Cancelar Evento</button>
-            <button className="adicionar-evento">Adicionar Evento</button>
+            <button className="cancelar-evento">
+                <img src= {cancela} alt="cancelar evento" className="cancelar-evento-img"></img>
+                <p>Cancelar Evento</p>
+            </button>
+
+            <button className="adicionar-evento">
+                <img src= {correto} alt="confirmar evento" className="confirmar-evento-img"></img>
+                <p>Adicionar Evento</p>
+            </button>
         </div>
         </div>
 
